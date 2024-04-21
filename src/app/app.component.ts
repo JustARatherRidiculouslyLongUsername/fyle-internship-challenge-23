cd62491392c5dc142a6c3b1319f714d60abf096e
Viswas Haridas <viswas.b10@gmail.com>
2024-04-21 06:30:23 +0530

    chore: migrate to standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
 
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from './search/search-bar/search-bar.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, SearchBarComponent],
 })
-export class AppComponent implements OnInit{
-  constructor(
-    private apiService: ApiService
-  ) {}
+export class AppComponent implements OnInit {
+  title = 'fyle-frontend-challenge';
+
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+    // this.apiService.getUser('johnpapa').subscribe(console.log);
   }
 }
